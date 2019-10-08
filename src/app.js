89aab36ad35b18f58c71314070390cc8e22d6afd
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-08 18:13:38 -0300

    Improve security and set limits

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
-import 'dotenv/config';
+import 'express-async-errors';
+import cors from 'cors';
 import Express from 'express';
+import helmet from 'helmet';
+import Limit from 'express-rate-limit';
+import LimitRedis from 'rate-limit-redis';
 import path from 'path';
+import redis from 'redis';
 import * as Sentry from '@sentry/node';
 
 import './database';
@@ -18,12 +23,32 @@ class App {
   }
 
   middlewares() {
-    this.server.use(cors());
+    this.server.use(helmet());
+    this.server.use(
+      cors({
+        origin: process.env.APP_URL,
+      })
+    );
     this.server.use(Express.json());
     this.server.use(
       '/files',
       Express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new Limit({
+          max: 100,
+          store: new LimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15, // 15m
+        })
+      );
+  }
   }
 
   routes() {
