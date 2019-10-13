fccdc45d78f9fdf4606b12a1150c5e6bd6eb15c1
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-12 23:52:35 -0300

    Update the file upload test

--- __tests__/integration/file.test.js
+++ __tests__/integration/file.test.js
@@ -1,45 +1,32 @@
-import request from 'supertest';
-import jwt from 'jsonwebtoken';
-import path from 'path';
 import fs from 'fs';
+import path from 'path';
+import request from 'supertest';
+
 import app from '../../src/app';
-import User from '../../src/app/models/User';
-import { SECRET, EXPIRATION_TIME } from '../../src/config/auth';
+import factory from '../utils/factories';
+import jwtoken from '../utils/jwtoken';
 
 describe('File', () => {
-  let token;
+  it('should be able to register a file', async () => {
+    const { id } = await factory.create('User');
+    const token = jwtoken(id);
 
-  beforeAll(async () => {
-    const { id } = await User.create({
-      name: 'user',
-      email: 'user10@test.com',
-      password: '123456',
-    });
-
-    token = jwt.sign({ id }, SECRET, {
-      expiresIn: EXPIRATION_TIME,
-    });
-  });
-
-  it('should be able to register a file', () => {
     const filen_name = 'ee59117ef6837153c44d7793ea3639c2.jpg';
     const file_path = path.resolve(__dirname, '..', 'files', filen_name);
 
-    fs.exists(file_path, async exists => {
-      if (exists) {
-        const response = await request(app)
-          .post('/files')
-          .set('Authorization', `Bearer ${token}`)
-          .attach('file', file_path);
+    if (fs.existsSync(file_path)) {
+      const response = await request(app)
+        .post('/files')
+        .set('Authorization', `Bearer ${token}`)
+        .attach('file', file_path);
 
-        expect(response.body).toMatchObject({
-          id: expect.any(Number),
-          url: expect.any(String),
-          name: filen_name,
-        });
-      } else {
-        throw Error('File does not exists!');
-      }
-    });
+      expect(response.body).toMatchObject({
+        id: expect.any(Number),
+        url: expect.any(String),
+        name: filen_name,
+      });
+    } else {
+      throw Error('File does not exists!');
+    }
   });
 });
