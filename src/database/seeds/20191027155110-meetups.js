a22fb66972bde3159734085bd9e497b5bba336f1
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-27 14:21:45 -0300

    Create past and future meetups

--- src/database/seeds/20191027155110-meetups.js
+++ src/database/seeds/20191027155110-meetups.js
@@ -11,11 +11,26 @@ module.exports = {
     const users_id = users[0].map(user => user.id);
     const banners_id = banners[0].map(banner => banner.id);
 
-    for (let i = 0; i < 40; i += 1) {
+    for (let i = 0; i < 5; i += 1) {
       meetups.push({
         banner_id: banners_id[Math.floor(Math.random() * banners_id.length)],
         created_at: new Date(),
-        date: faker.date.recent(),
+        date: faker.date.past(),
+        description: faker.lorem.paragraphs(2),
+        localization: faker.address.streetAddress(),
+        title: faker.name.title(),
+        updated_at: new Date(),
+        user_id: users_id[Math.floor(Math.random() * users_id.length)],
+      });
+    }
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    for (let i = 0; i < 35; i += 1) {
+      meetups.push({
+        banner_id: banners_id[Math.floor(Math.random() * banners_id.length)],
+        created_at: new Date(),
+        date: faker.date.between(new Date(), tomorrow),
         description: faker.lorem.paragraphs(2),
         localization: faker.address.streetAddress(),
         title: faker.name.title(),
