e91bed16ae98283d3f0b86e2c22b78035580bab3
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-07 17:39:00 -0300

    Simplify initialization

--- src/database/index.js
+++ src/database/index.js
@@ -4,19 +4,15 @@ import File from '../app/models/File';
 import Meetup from '../app/models/Meetup';
 import Subscription from '../app/models/Subscription';
 import User from '../app/models/User';
+
 import database from '../config/database';
 
-class Database {
-  constructor() {
-    this.init();
-  }
+const postgres = new Sequelize(database);
 
-  init() {
-    this.postgres = new Sequelize(database);
-    [User, File, Meetup, Subscription]
-      .map(model => model.init(this.postgres))
-      .map(model => model.associate && model.associate(this.postgres.models));
-  }
-}
-
-export default new Database();
+export default () => {
+  [User, File, Meetup, Subscription]
+    .map(model => model.init(postgres))
+    .map(model => {
+      return model.associate && model.associate(postgres.models);
+    });
+};
