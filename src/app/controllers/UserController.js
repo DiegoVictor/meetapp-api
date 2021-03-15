6fd88732f6303df99e8e47f56a74054547e22bcf
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-03-15 18:55:34 -0300

    Instantiate and use service

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,9 +3,11 @@ import { badRequest } from '@hapi/boom';
 import User from '../models/User';
 import EmailAlreadyUsed from '../services/EmailAlreadyUsed';
 
+const emailAlreadyUsed = new EmailAlreadyUsed();
+
 class UserController {
   async store(req, res) {
-    await EmailAlreadyUsed.run({ email: req.body.email });
+    await emailAlreadyUsed.execute({ email: req.body.email });
 
     const { id, email, name } = await User.create(req.body);
     return res.json({ id, email, name });
@@ -16,7 +18,7 @@ class UserController {
     const user = await User.findByPk(req.userId);
 
     if (email && email !== user.email) {
-      await EmailAlreadyUsed.run({ email });
+      await emailAlreadyUsed.execute({ email });
     }
 
     if (old_password && !(await user.checkPassword(old_password))) {
@@ -24,6 +26,7 @@ class UserController {
     }
 
     const { id, name } = await user.update(req.body);
+
     return res.json({ id, email, name });
   }
 }
