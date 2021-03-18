4023ba2114743d175d9a65ef98c5bb5fa3008fe7
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-03-18 18:16:37 -0300

    Configure HATEOAS

--- src/app/controllers/ScheduledController.js
+++ src/app/controllers/ScheduledController.js
@@ -6,10 +6,20 @@ const meetupExists = new MeetupExists();
 
 class ScheduledController {
   async index(req, res) {
+    const { currentUrl, userId } = req;
+
     const where = { user_id: userId };
     const meetups = await Meetup.findAll({ where });
     const count = await Meetup.count({ where });
+
     res.header('X-Total-Count', count);
+
+    return res.json(
+      meetups.map(meetup => ({
+        ...meetup.toJSON(),
+        url: `${currentUrl}/${meetup.id}`,
+      }))
+    );
   }
 
   async show(req, res) {
