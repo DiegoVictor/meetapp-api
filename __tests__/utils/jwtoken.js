238ce21f760ecc1a00420621d91ead7420b270fc
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-03-26 18:26:02 -0300

    Remove old tests

--- __tests__/utils/jwtoken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-import { EXPIRATION_TIME, SECRET } from '../../src/config/auth';
-
-export default id => {
-  return jwt.sign({ id }, SECRET, {
-    expiresIn: EXPIRATION_TIME,
-  });
-};
