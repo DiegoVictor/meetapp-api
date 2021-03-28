53b7908096477af8b1d954abc92d5ddacb946693
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-03-28 15:27:28 -0300

    Create factories

---
+++ tests/utils/factory.js
@@ -0,0 +1,33 @@
+import { factory } from 'factory-girl';
+import faker from 'faker';
+
+import File from '../../src/app/models/File';
+import Meetup from '../../src/app/models/Meetup';
+import User from '../../src/app/models/User';
+import Subscription from '../../src/app/models/Subscription';
+
+factory.define('File', File, {
+  name: faker.image.image,
+  path: faker.random.uuid,
+});
+
+factory.define('Meetup', Meetup, {
+  banner_id: null,
+  date: faker.date.future,
+  description: () => faker.lorem.paragraphs(1).slice(0, 254),
+  localization: faker.address.streetAddress,
+  title: faker.name.title,
+});
+
+factory.define('Subscription', Subscription, {
+  meetup_id: null,
+  user_id: null,
+});
+
+factory.define('User', User, {
+  email: faker.internet.email,
+  name: faker.name.findName,
+  password: faker.internet.password,
+});
+
+export default factory;
