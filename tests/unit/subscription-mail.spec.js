d24c04515071add99755f7c6b719ee41b686f460
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-03-29 19:46:02 -0300

    Test queue

---
+++ tests/unit/subscription-mail.spec.js
@@ -0,0 +1,46 @@
+import faker from 'faker';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import factory from '../utils/factory';
+import SubscriptionMail from '../../src/app/jobs/SubscriptionMail';
+import { sendMail } from '../../mocks/nodemailer';
+
+describe('SubscriptionMail', () => {
+  it('should be able to send the email', async () => {
+    const { email, name } = await factory.attrs('User');
+    const meetup = {
+      banner: {
+        url: faker.image.imageUrl(),
+      },
+      date: faker.date.future().toISOString(),
+      title: faker.name.title(),
+    };
+
+    const subscriptionMail = new SubscriptionMail();
+    await subscriptionMail.handle({
+      data: {
+        meetup,
+        user: {
+          email,
+          name,
+        },
+      },
+    });
+
+    expect(sendMail).toHaveBeenCalledWith({
+      from: 'MeetApp <noreply@meetapp.com>',
+      to: `${name} <${email}>`,
+      subject: 'Nova inscrição',
+      template: 'subscription',
+      context: {
+        banner: meetup.banner.url,
+        date: format(parseISO(meetup.date), "dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+        meetup: meetup.title,
+        user: name,
+      },
+    });
+  });
+});
