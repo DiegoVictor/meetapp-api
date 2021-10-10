fdba00615ff162d4c8d83dea9ab168f3b58366f3
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-10 14:06:43 -0300

    Create documentation endpoint

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ import helmet from 'helmet';
 import path from 'path';
 import { isBoom } from '@hapi/boom';
 import { errors } from 'celebrate';
+import swagger from 'swagger-ui-express';
 
+import swaggerDocument from './swagger.json';
 import routes from './routes';
 import routeAliases from './app/middlewares/routeAliases';
 import database from './database';
@@ -24,6 +26,7 @@ app.use(
   express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
 );
 
+app.use('/docs', swagger.serve, swagger.setup(swaggerDocument));
 app.use(routeAliases);
 app.use('/v1', routes);
 
